ef075bd9b3d0d74890e8279702dc18244d976809
Codepro100 <codeficct05@gmail.com>
2024-11-27 07:55:04 -0400

    Add files via upload

---
+++ matriz SI (EJERCICIO1) - X/matriz/nent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz
+{
+    class nent
+    {
+        private int n;
+        public nent()
+        {
+            n = 0;
+        }
+        public void cargar(int dato)
+        {
+            n = dato;
+        }
+        public int descargar()
+        {
+            return n;
+        }
+        public bool veriffibo()
+        {
+            if (n == 0 || n == 1)
+                return true;
+
+            int fibonacciAnterior = 0;
+            int fibonacciActual = 1;
+
+            while (fibonacciActual <= n)
+            {
+                if (fibonacciActual == n)
+                    return true;
+
+                int siguienteFibonacci = fibonacciAnterior + fibonacciActual;
+                fibonacciAnterior = fibonacciActual;
+                fibonacciActual = siguienteFibonacci;
+            }
+
+            return false;
+        }
+    }
+}
